0338190fb45bece69f3642fdc435e521f5c170fa
Andhitia Rama <andhitia.r@gmail.com>
2022-05-07 09:17:44 +0700

    [ssi_hr_award] 14.0.1.2.0
    * Record rule implementation

--- ssi_hr_award/__manifest__.py
+++ ssi_hr_award/__manifest__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Employee Award",
-    "version": "14.0.1.1.0",
+    "version": "14.0.1.2.0",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "AGPL-3",
@@ -21,6 +21,7 @@
         "security/ir_module_category_data.xml",
         "security/res_group_data.xml",
         "security/ir.model.access.csv",
+        "security/ir_rule_data.xml",
         "data/ir_sequence_data.xml",
         "data/sequence_template_data.xml",
         "data/policy_template_data.xml",
