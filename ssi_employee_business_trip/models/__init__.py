3419a841d875b8062b1f455d02622a7326964e6e
usa8bit <usa8bit@gmail.com>
2023-07-27 17:32:05 +0700

    [ssi_employee_business_trip] 14.0.1.0.0

---
+++ ssi_employee_business_trip/models/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import (
+    employee_business_trip,
+    employee_business_trip_per_diem,
+    employee_business_trip_tax,
+    employee_business_trip_type,
+)
